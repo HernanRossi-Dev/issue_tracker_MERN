9e7a204f1108f0c32274039a5eebd7864b1a36a7
hernan <hernan_rossi@msn.com>
2017-12-19 18:47:38 -0800

    HMR implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+
 const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: './src/App.jsx',
+        app: ['./src/App.jsx'],
         vendor: ['react', 'react-dom', 'whatwg-fetch'],
     },
     output: {
-        path: __dirname + '/static',
-        filename: 'app.bundle.js'
+        path: __dirname + './static',
+        filename: 'app.bundle.js',
+    },
+    devServer: {
+        port: 8000,
+        contentBase: 'static',
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:3000'
+            }
+        }
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
